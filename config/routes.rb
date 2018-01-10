f1b6d437605ff3081766012d4ec5709828d4d451
Robert Laws <roblaws@me.com>
2018-01-10 18:30:38 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :authors, only: [:show, :new, :edit, :create, :update]
-  resources :post, only: [:show, :new, :edit, :create, :update]
+  resources :posts, only: [:show, :new, :edit, :create, :update]
 end
