57486be9390d5b0afad8b1fb65a84c565c6ee674
Robert Laws <roblaws@me.com>
2018-01-10 18:23:46 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -14,11 +14,11 @@ class AuthorsController < ApplicationController
   end
 
   # def edit
-  #   @author = Author.find(params[:id])
-  # end
-  # 
-  # def update
   #   
+  # end
+  #
+  # def update
+  #
   # end
 
   private
